dbd082ee3e734d2f64f191492139ca3e68c0b659
Keiichiro Ui <keiichiro.ui@gmail.com>
2014-12-15 19:55:33 +0900

    Add functions to set and view cookies

--- web.rb
+++ web.rb
@@ -2,6 +2,39 @@ require 'sinatra'
 require 'sinatra/cookies'
 
 get '/' do
-  cookies[:foo] = "bar"
-  "Hello, world"
+  <<EOH
+<title>3rd Cookie Block test on IE</title>
+<h1>3rd Cookie Block test on IE</h1>
+
+<ul>
+<li><a href="/set-cookie">set cookies</a>
+<li><a href="/view-cookie">view cookies</a>
+</ul>
+EOH
+end
+
+get '/set-cookie' do
+  cookies[:foo] = "123"
+  cookies[:bar] = "456"
+  <<EOH
+<title>Set Cookies</title>
+<h1>Set Cookies</h1>
+
+<p>Set cookies: <code>foo=123</code> and <code>bar=456</code>
+EOH
+end
+
+get '/view-cookie' do
+  c = cookies.reduce('') do |result, (k, v)|
+    result + "<tr><th>#{k}</th><th>#{v}</th><tr>\n"
+  end
+  <<EOH
+<title>View Cookies</title>
+<h1>View Cookies</h1>
+
+<table>
+<tr><th>Name</th><th>Value</th></tr>
+#{c}
+</table>
+EOH
 end
