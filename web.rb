780bd2c8aeecc87fbf6bdea2951ef11865dcc0a8
Keiichiro Ui <keiichiro.ui@gmail.com>
2014-12-15 19:59:02 +0900

    Mod to be usable

--- web.rb
+++ web.rb
@@ -21,20 +21,24 @@ get '/set-cookie' do
 <h1>Set Cookies</h1>
 
 <p>Set cookies: <code>foo=123</code> and <code>bar=456</code>
+
+<p><a href="/">Top</a>
 EOH
 end
 
 get '/view-cookie' do
   c = cookies.reduce('') do |result, (k, v)|
-    result + "<tr><th>#{k}</th><th>#{v}</th><tr>\n"
+    result + "<tr><th>#{k}</th><td>#{v}</td><tr>\n"
   end
   <<EOH
 <title>View Cookies</title>
 <h1>View Cookies</h1>
 
-<table>
+<table border="1">
 <tr><th>Name</th><th>Value</th></tr>
 #{c}
 </table>
+
+<p><a href="/">Top</a>
 EOH
 end
